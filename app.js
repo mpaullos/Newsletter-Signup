8e754504dc3a91fcd8ec7494c0730c96e9b56d9f
Marcos Paulo <82289818+mpaullos@users.noreply.github.com>
2022-12-14 23:34:41 -0300

    feat: code works

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
-
 const bodyParser = require("body-parser");
-
+const https = require("https");
+const { request } = require("http");
 const app = express();
-
 const port = 3000;
+
 
 app.use(express.static("public"));
 
@@ -12,6 +12,42 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us21.api.mailchimp.com/3.0/lists/e7cabb6370";
+
+  const options = {
+    method: "post",
+    auth: "marcos:d174f1c7afdb6a57400a066881982f91-us21",
+  };
+
+  const request = https.request(url, options, (response) => {
+    response.on("data", (data) => {
+      console.log(JSON.parse(data));
+    });
+  });
+  
+  request.write(jsonData);
+  request.end();
 });
 
 app.listen(port, () => {
